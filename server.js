3fe12bce4f35dcdbb8358903fe857dbefacdb375
Kat Imperial <imperialkatherine@gmail.com>
2022-03-29 15:33:02 -0400

    configure for heroku deployment

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(bodyParser.json())
+app.use(express.static(`${__dirname}/client/build`))
 
 // app.use() middleware here ^ ///////////////////
 
 app.use('/api', routes);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+   })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
