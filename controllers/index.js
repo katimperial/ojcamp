c8e57cb1a42ef1cc6972d787eeb08cff12abe9ab
Kat Imperial <imperialkatherine@gmail.com>
2022-03-29 12:02:10 -0400

    did GET for projects

--- controllers/index.js
+++ controllers/index.js
@@ -1,35 +1,14 @@
-const Album = require('../models/album');
-
-const createAlbum = async (req, res) => {
-    try {
-        const album = await new Album(req.body)
-        await album.save()
-        return res.status(201).json({
-            album,
-        });
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-}
-
-module.exports = {
-    createAlbum,
-}
-
 const Project = require('../models/project');
 
-const createProject = async (req, res) => {
+const getProjects = async (req, res) => {
     try {
-        const project = await new Project(req.body)
-        await project.save()
-        return res.status(201).json({
-            project,
-        });
+        const projects = await Project.find()
+        return res.status(200).json({ projects })
     } catch (error) {
-        return res.status(500).json({ error: error.message })
+        return res.status(500).send(error.message);
     }
 }
 
 module.exports = {
-    createProject,
+    getProjects,
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
+const controllers = require('../controllers')
 const router = Router();
 
 router.get('/', (req, res) => res.send('This is OJcamp root!'))
+
+router.get('/projects', controllers.getProjects)
 
 module.exports = router;
