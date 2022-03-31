611dc9e4d50db5062ea90c35e47ac8beca8db62c
Kat Imperial <imperialkatherine@gmail.com>
2022-03-31 16:06:05 -0400

    hopefully fixed react for the last time

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios'
-import { Routes, Route, useRoutes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Nav from './components/Nav';
