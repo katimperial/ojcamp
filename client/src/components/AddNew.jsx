be7ed382c04d9ab74154cd42779a63837916884c
Kat Imperial <imperialkatherine@gmail.com>
2022-04-01 09:34:03 -0400

    added basic form to AddNew and some CSS

--- client/src/components/AddNew.jsx
+++ client/src/components/AddNew.jsx
@@ -1,5 +1,35 @@
-const AddNew = () => {
+import React from "react"
 
+const AddNew = () => {
+    return (
+        <div className="newMusic">
+            <h3>Add New Music</h3>
+            <label>
+                Name:
+                <input type="text"/>
+            </label>
+            <label>
+                Album Title:
+                <input type="text"/>
+            </label>
+            <label>
+                Track Listings:
+                <input type="text"/>
+            </label>
+            <label>
+                Release Date:
+                <input type="text"/>
+            </label>
+            <label>
+                Label:
+                <input type="text"/>
+            </label>
+            <label>
+                Album Art URL:
+                <input type="text"/>
+            </label>
+        </div>
+    )
 }
 
 export default AddNew
