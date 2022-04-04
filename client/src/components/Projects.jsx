97471475f9198e2a4904c10a52ad9cc1271a9023
Kat Imperial <imperialkatherine@gmail.com>
2022-04-04 01:24:51 -0400

    moved albums out of map

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -16,10 +16,6 @@ const Projects = (props) => {
                 // console.log(props.project.id)
                 props.projects.map((project) => (
                     <div key={project.id} className="card">
-                        {/* {displayAlbum === false ?
-                        <h2></h2> : 
-                            <Albums displayAlbum={displayAlbum} /> } */}
-                        {displayAlbum ? <Albums /> : null}
                         <h3>{project.name}</h3>
                         <img src={project.image} className="projectImage"/>
                         <h4>{project.albums}</h4>
@@ -27,6 +23,8 @@ const Projects = (props) => {
                 ))
 
             }
+            {displayAlbum ? <Albums /> : null}
+
             <button onClick={toggleAlbums}>{displayAlbum === false ? "View Albums" : "Clear Albums"} 
             </button>
         </div>
